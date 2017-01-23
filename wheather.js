3d4f37cd11506d0692fd8dc87757aeaea74461fa
Anuj-786 <anuj.k@gai.co.in>
2017-01-23 14:34:03 +0530

    Working Version: Adding the styling for wheather

--- wheather.js
+++ wheather.js
@@ -1,55 +1,53 @@
 var lat;
 var long;
 function mapbox() {
- L.mapbox.accessToken = 'pk.eyJ1IjoiYW51ai03ODYiLCJhIjoiY2lwcWd4cXltMDYyaWZ1bTM0cHFmaGhucSJ9.x3NgVTbDzK_K8tCZTFG-pg';
+  L.mapbox.accessToken = 'pk.eyJ1IjoiYW51ai03ODYiLCJhIjoiY2lwcWd4cXltMDYyaWZ1bTM0cHFmaGhucSJ9.x3NgVTbDzK_K8tCZTFG-pg';
 
- var map = L.mapbox.map('map', 'mapbox.streets');
+  var map = L.mapbox.map('map', 'mapbox.streets');
 
- map.locate({setView: true, maxZoom: 10});
+  map.locate({setView: true, maxZoom: 10});
 
-              // Create a featureLayer that will hold a marker and linestring.
-              var featureLayer = L.mapbox.featureLayer().addTo(map);
-              var secondFeatureLayer = L.mapbox.featureLayer().addTo(map);
-              
-              map.locate();
+    // Create a featureLayer that will hold a marker and linestring.
+    var featureLayer = L.mapbox.featureLayer().addTo(map);
+    var secondFeatureLayer = L.mapbox.featureLayer().addTo(map);         
 
-              map.on('locationfound', function(e) {
+    map.locate();
 
-              	map.fitBounds(e.bounds);
+    map.on('locationfound', function(e) {
 
-              	featureLayer.setGeoJSON({
-              		type: 'Feature',
-              		geometry: {
-              			type: 'Point',
-              			coordinates: [e.latlng.lng, e.latlng.lat]
-              		},
-              		properties: {
-              			'title': 'Here I am!',
-              			'marker-color': '#ff8888',
-              			'marker-symbol': 'star'
-              		}
-              		
-              	});
-              	long = e.latlng.lng;
-              	lat = e.latlng.lat;
-              		//console.log("longitude: " + " "+ lat + "latitude: " + " "+ long);
+     	map.fitBounds(e.bounds);
 
-            	// And hide the geolocation button
-            	//geolocate.parentNode.removeChild(geolocate);
-            });
-            }
-            mapbox();
-            function gettingJSON(){
-             
-             console.log("longitude: " + " "+ lat + "latitude: " + " "+ long);
-             document.write("jquery loaded");
-          	//
-          	console.log("http://api.openweathermap.org/data/2.5/weather?lat="+ lat+"&lon="+long+"&appid=dd5262af1afc2fe8ddfa572282ed0691")
+      featureLayer.setGeoJSON({
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [e.latlng.lng, e.latlng.lat]
+        },
+        properties: {
+          'title': 'Here I am!',
+          'marker-color': '#ff8888',
+          'marker-symbol': 'star'
+     		}
+      });
+      long = e.latlng.lng;  
+     	lat = e.latlng.lat;
+      //console.log("longitude: " + " "+ lat + "latitude: " + " "+ long);
 
-          	$.getJSON("http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+long+"&appid=dd5262af1afc2fe8ddfa572282ed0691",function(json){
+      //And hide the geolocation button
+              	//geolocate.parentNode.removeChild(geolocate);
+    });
+}
+mapbox();
+function gettingJSON(){
+  
+  $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+long+"&appid=dd5262af1afc2fe8ddfa572282ed0691",function(json){
           		// console.log(JSON.stringify(json));
-          		var temperature = json.main.temp - 273.15;
-          		alert (temperature + " " +"\xB0C")
+    var temperature = json.main.temp - 273.15;
+    document.getElementById("city").innerHTML = json.name;
+    document.getElementById("temperature").innerHTML = "<h2>" +temperature+ " " +"\xB0C" + "</h2>";
 
-          	});
-          }
+    //alert (temperature + " " +"\xB0C")
+
+  });
+  return null;
+}
